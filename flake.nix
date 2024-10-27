02df86cc226393b661bd4a1bf9c78a960eac8ddf
Denis Bekasow <denis.bekasow@yandex.com>
2024-10-27 19:01:57 +0000

    refactor: simiplify flake

--- flake.nix
+++ flake.nix
@@ -7,64 +7,41 @@
 
     zmk-nix.url = "github:lilyinstarlight/zmk-nix";
     zmk-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
   };
 
   outputs = inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.git-hooks-nix.flakeModule
-        ./pkgs
-      ];
+      imports = [ ./pkgs ];
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { config, lib, pkgs, self', inputs', ... }:
-        let
-          inherit (inputs') zmk-nix;
-        in
-        {
-          packages = rec {
-            firmware = zmk-nix.legacyPackages.buildSplitKeyboard {
-              name = "sweep-firmware";
+      perSystem = { config, lib, pkgs, self', inputs', ... }: {
+        packages = {
+          firmware = inputs'.zmk-nix.legacyPackages.buildSplitKeyboard {
+            name = "sweep-firmware";
 
-              src = lib.sourceFilesBySuffices ./. [ ".conf" ".dtsi" ".h" ".keymap" ".yml" ];
+            src = lib.sourceFilesBySuffices ./. [ ".conf" ".dtsi" ".h" ".keymap" ".yml" ];
 
-              board = "nice_nano_v2";
-              shield = "splitkb_aurora_sweep_%PART%";
+            board = "nice_nano_v2";
+            shield = "splitkb_aurora_sweep_%PART%";
 
-              zephyrDepsHash = "sha256-Xfu6eWmAOE/ZcY5XszdD0YHaGK1eIoydcd5TTh8spPw=";
+            zephyrDepsHash = "sha256-Xfu6eWmAOE/ZcY5XszdD0YHaGK1eIoydcd5TTh8spPw=";
 
-              meta = with lib; {
-                description = "Firmware for my sweep keyboard";
-                platforms = platforms.all;
-                license = licenses.mit;
-              };
-            };
-            flash = zmk-nix.packages.flash.override { inherit firmware; };
-            inherit (zmk-nix.packages) update;
-
-            draw-keymap = pkgs.callPackage ./lib/draw-keymap.nix {
-              inherit (self'.packages) keymap-drawer;
-              src = ./.;
-              keymap = "splitkb_aurora_sweep.keymap";
+            meta = with lib; {
+              description = "Firmware for my sweep keyboard";
+              platforms = platforms.all;
+              license = licenses.mit;
             };
           };
-          pre-commit = {
-            check.enable = true;
-            settings.hooks = {
-              commitizen.enable = true;
-              nixpkgs-fmt.enable = true;
-              typos.enable = true;
-              yamlfmt.enable = true;
-            };
-          };
-          devShells.default = pkgs.mkShell {
-            name = "zmk-config";
-            inputsFrom = [
-              config.pre-commit.devShell
-              zmk-nix.devShells.default
-            ];
+          flash = inputs'.zmk-nix.packages.flash.override { inherit (config.packages) firmware; };
+          inherit (inputs'.zmk-nix.packages) update;
+
+          draw-keymap = pkgs.callPackage ./lib/draw-keymap.nix {
+            inherit (self'.packages) keymap-drawer;
+            src = ./.;
+            keymap = "splitkb_aurora_sweep.keymap";
           };
         };
+
+        devShells.default = inputs'.zmk-nix.devShells.default;
+      };
     };
 }
